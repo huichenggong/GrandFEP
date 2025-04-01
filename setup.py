46dfaa5e06e4c949f8fefc8b3b0153ee9d888271
Chenggong <chenggong.hui@mpinat.mpg.de>
2025-04-01 21:19:16 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import codecs
+import os.path
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+setup(
+    name='grandfep',
+    version=get_version("grandfep/__init__.py"),
+    author='Chenggong Hui',
+    author_email='chenggong.hui@mpinat.mpg.de',
+    description='GCMC/MD with free energy perturbation',
+    packages=find_packages(),
+    install_requires=["pymbar>=3,<5",
+                      "numpy",
+                      "pandas",
+                      "matplotlib",
+                      "scipy>=1.7.0",
+                      "openmm", "openmmtools",
+                      "parmed",
+                      "mpi4py"
+                      ],
+    python_requires='>=3.11',
+      entry_points={
+          'console_scripts': [
+          'grand_RE_MPI=grandfep.grand_RE_MPI:main',
+          ],
+      },
+    classifiers=['Programming Language :: Python :: 3',],
+)
