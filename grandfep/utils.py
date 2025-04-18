ba9bd26687b251f13e1f17f2221871b61ccdc383
Chenggong <chenggong.hui@mpinat.mpg.de>
2025-04-18 21:54:28 +0200

    update doc

--- grandfep/utils.py
+++ grandfep/utils.py
@@ -1,20 +1,30 @@
 import gzip
 from pathlib import Path
+from typing import Union
 
 from openmm import app, openmm
 
 from .relative import HybridTopologyFactory
 
 
-def load_sys(sys_file):
+def load_sys(sys_file: Union[str, Path]) -> openmm.System:
     """
-    Load serialized system file
+    Load a serialized OpenMM system from a xml or xml.gz file.
 
-    :param sys_file: (str or pathlib.Path)
+    Parameters
+    ----------
+    sys_file
+        Path to the serialized OpenMM system file (.xml or .xml.gz).
 
-        X.xml.gz or X.xml. Serialized system file.
+    Returns
+    -------
+    openmm.System
+        The deserialized OpenMM System object.
 
-    :return: openmm.System
+    Examples
+    --------
+    >>> from grandfep import utils
+    >>> system = utils.load_sys("system.xml.gz")
 
     """
     # if sys_file is xml.gz
@@ -26,21 +36,30 @@ def load_sys(sys_file):
             system = openmm.XmlSerializer.deserialize(f.read())
     return system
 
-def load_top(top_file):
+def load_top(top_file: Union[str, Path]) -> tuple[app.topology.Topology, Union[app.CharmmPsfFile, app.AmberPrmtopFile]]:
     """
-    Load topology file. It can be psf(Charmm) or parm7/prmtop(Amber). remember to run topology.setPeriodicBoxVectors.
-    if you provide a charmm psf file. If you don't set the box for topology, later trajectory will not have box information.
+    Load a topology file in PSF (CHARMM) or PRMTOP/PARM7 (AMBER) format.
 
-    :param top_file: (str or pathlib.Path)
+    After loading a top file, you should call `.setPeriodicBoxVectors()` on the topology object
+    to define the box. Without setting the box, trajectory files may lack box information.
 
-        psf or parm7/prmtop file
+    Parameters
+    ----------
+    top_file
+        Path to the topology file (either .psf, .prmtop, or .parm7).
 
-    :return: (topology, psf/prmtop)
+    Returns
+    -------
+    topology : openmm.app.Topology
+        The OpenMM Topology object for use in simulation setup.
 
-        openmm.Topology: The OpenMM topology object.
+    top_object : openmm.app.CharmmPsfFile or openmm.app.AmberPrmtopFile
+        The loaded OpenMM file object used to construct the topology.
 
-        Union[openmm.app.PSFFile, openmm.app.AmberPrmtopFile]: The original OpenMM topology file object.
-
+    Raises
+    ------
+    ValueError
+        If the file format is not supported. Only the extensions .psf, .prmtop, and .parm7 are supported.
     """
     top_file = str(top_file)
     if top_file.endswith(".psf"):
